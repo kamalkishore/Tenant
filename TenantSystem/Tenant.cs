8e4151df095a444a5ddcbaff83bbcee5be688d7f
kamalkishore <kamal_16june@yahoo.co.in>
2014-05-03 23:23:07 +0530

    Just Testing

---
+++ TenantSystem/Tenant.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenantSystem
+{
+    public class Tenant
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public void Save(string conString)
+        {
+            //SqlConnection con = new SqlConnection(conString);
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO TenantDetails (FirstName,Lastname, PhoneNumber) VALUES (@fName,@lName, @PhoneNo)");
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = connection;
+                cmd.Parameters.AddWithValue("@fName", FirstName);
+                cmd.Parameters.AddWithValue("@lName", LastName);
+                cmd.Parameters.AddWithValue("@PhoneNo", PhoneNumber);
+                //cmd.Parameters.AddWithValue("@Address", txtAddress.Text);
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ WebApplication1/SubmitTenantDetails.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="SubmitTenantDetails.aspx.cs" Inherits="WebApplication1.WebForm2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div>
+        <div>Fill Tenant Details</div>
+        <div>First Name</div>
+        <div>
+            <asp:TextBox ID="txtFirstName" runat="server"></asp:TextBox>
+        </div>
+        <div>Last Name</div>
+        <div>
+            <asp:TextBox ID="txtLastName" runat="server"></asp:TextBox>
+        </div>
+        <div>Phone Number</div>
+        <div>
+            <asp:TextBox ID="txtPhoneNumber" runat="server"></asp:TextBox>
+        </div>
+        
+        <div>
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit Details" OnClick="btnSubmit_Click" /></div>
+    </div>
+</asp:Content>
